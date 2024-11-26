7293ff38660bbb8e4f5333426ec4b59060a36f75
BarsatKhadka <khadkabarsat598@gmail.com>
2024-11-26 13:35:12 -0600

    way to add api key securely

--- MovieDatabaseFetch/src/main/java/CLI/MovieDatabaseFetch/Service/getTitleService.java
+++ MovieDatabaseFetch/src/main/java/CLI/MovieDatabaseFetch/Service/getTitleService.java
@@ -2,6 +2,8 @@ package CLI.MovieDatabaseFetch.Service;
 
 
 import CLI.MovieDatabaseFetch.models.RequestAPIModel;
+import CLI.MovieDatabaseFetch.models.Result;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpMethod;
@@ -15,15 +17,22 @@ public class getTitleService {
 
     public RestTemplate restTemplate = new RestTemplate();
 
+    @Value("${spring.TMDB.apiKey}")
+    private String apiKey;
+
     HttpHeaders headers = new HttpHeaders();
 
     public void getPopularTitle(){
         String url = "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&sort_by=popularity.desc";
         headers.set("accept", "application/json");
-        headers.set("Authorization", "Bearer {your_api_token}");
+        headers.set("Authorization", "Bearer "+ apiKey);
         HttpEntity<String> entity = new HttpEntity<>(headers);
         ResponseEntity<RequestAPIModel> responseEntity = restTemplate.exchange(url, HttpMethod.GET, entity, RequestAPIModel.class);
-        System.out.println(responseEntity.getBody());
+        if(responseEntity.getBody() != null && responseEntity.getBody().getAllResults() != null){
+            for(Result result : responseEntity.getBody().getAllResults()){
+                System.out.println(result.getMovietitle());
+            }
+        }
     }
 
 
