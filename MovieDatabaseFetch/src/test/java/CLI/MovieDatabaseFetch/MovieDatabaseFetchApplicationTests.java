57c1bb334ed4396f7f35cb3c80213e78e5586f46
Barsat Khadka <khadkabrsat598@gmail.com>
2024-11-25 18:37:41 -0600

    Project start by implementing command line runner

---
+++ MovieDatabaseFetch/src/test/java/CLI/MovieDatabaseFetch/MovieDatabaseFetchApplicationTests.java
@@ -0,0 +1,13 @@
+package CLI.MovieDatabaseFetch;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieDatabaseFetchApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
